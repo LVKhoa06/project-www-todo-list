05b445b4e893b2220f35abcfbc20c576f28bdf35
KhoaPC <luongvankhoapc@gmail.com>
2022-08-09 10:50:51 +0700

    22Aug09 ADD func dragElm

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -185,3 +185,46 @@ function getDeadline(arrTime) {
         text
     }
 } // getDeadline 
+
+
+function dragElm(elm) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    if (document.querySelector(elm.id || elm.className)) {
+        /* if present, the header is where you move the DIV from:*/
+        document.querySelector(elm.id || elm.className).onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elm.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elm.style.top = (elm.offsetTop - pos2) + "px";
+        elm.style.left = (elm.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+} // drag elm
