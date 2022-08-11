cd291d646f73a6300717b0f61fa4b99eaf8c1bf4
KhoaPC <luongvankhoapc@gmail.com>
2022-08-11 15:48:53 +0700

    22Aug11 MISC

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,4 +1,6 @@
-
+// mobile / responsive / drag drop
+// pin / unpin
+// style lung linh
 const monthM31 = [1, 3, 5, 7, 8, 10, 12];
 const monthSpecial = 2;
 let years;
@@ -57,7 +59,7 @@ function getTimeToday() {
         Number(editTime(month)),
         year
     ];
-} // getTime2
+} // getTimeToday
 
 function totalDay(dateString) {
     const secondsInADay = 24 * 60 * 60;
@@ -66,7 +68,6 @@ function totalDay(dateString) {
     const deltaInSeconds = (stampOfInput - stampOfToday) / 1000;
 
     return Math.ceil(deltaInSeconds / secondsInADay);
-    // deadlineInDays
 } // totalDay
 
 function getDayName() {
@@ -185,48 +186,6 @@ function getDeadline(arrTime) {
         text
     }
 } // getDeadline 
-
-function dragElm(elm) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-    if (document.querySelector(elm.id || elm.className)) {
-        /* if present, the header is where you move the DIV from:*/
-        document.querySelector(elm.id || elm.className).onmousedown = dragMouseDown;
-    } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        elm.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elm.style.top = (elm.offsetTop - pos2) + "px";
-        elm.style.left = (elm.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-} // drag elm
 
 function moveItem(arr, fromIndex, toIndex) {
     const item = arr.at(fromIndex);
